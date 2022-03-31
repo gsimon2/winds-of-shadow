64aaa02e9de70c462f2421a65743c2842daa8371
Glen Simon <glen.a.simon@gmail.com>
2022-03-31 11:40:31 -0400

    Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-import { Box, Container, Grid } from '@mui/material';
+import { Box, Container, Grid, Grow, SxProps, useMediaQuery } from '@mui/material';
 import titleBackground from './assets/misty_mountains.jpg';
-import sectionTwoBackground from './assets/cloudy-heavens.jpg';
 import bookCover from './assets/book-cover.jpg';
 import authorPhoto from './assets/author-photo.jpg';
+import cavern from './assets/cavern.jpg';
+import darkForest from './assets/dark-forest.webp';
 import React, { createRef, useState } from 'react';
 
 const parallax: React.CSSProperties = {
@@ -20,8 +21,8 @@ const title: React.CSSProperties = {
   justifyContent: 'center',
   maxWidth: '100% !important',
   fontFamily: 'serif',
-  fontSize: '4rem',
   margin: '0 1rem',
+  textAlign: 'center'
 }
 
 const nonStickyTitle: React.CSSProperties = {
@@ -47,7 +48,19 @@ const descriptionContainer: React.CSSProperties = {
   fontSize: '1.5rem'
 }
 
+const photoCircle: SxProps = {
+  height: '400px',
+  width: '400px',
+  overflow: 'hidden',
+  borderRadius: '50%',
+
+  'img': {
+    marginLeft: '-12%' // Should be -25% for centered photos
+  }
+}
+
 function App() {
+  const isMobileView = useMediaQuery(`(max-width:900px)`);
   const [isStickyTitle, setIsStickyTitle] = useState(false);
   const titleRef = createRef<HTMLDivElement>();
   const amazonUrl = "https://www.amazon.com/Winds-Shadow-Wind-Whispers-Book/dp/B096HS1XM1";
@@ -71,11 +84,16 @@ function App() {
   return (
     <Box sx={{height: '100%', width: '100%', overflow: 'auto'}} onScroll={handleScroll}>
       <Box sx={{...parallax, backgroundImage: `url(${titleBackground})`}}>
-        <Box sx={{ ...title, ...centerContent, ...(isStickyTitle ? stickyTitle : nonStickyTitle)}} ref={titleRef}>
-          <h1 style={{margin: 0}}>
-            Winds Of Shadow
-          </h1>
-        </Box>
+          <Box sx={{ ...title, ...centerContent, ...(isStickyTitle ? stickyTitle : nonStickyTitle), flexDirection: 'column'}} ref={titleRef}>
+            <Grow in={true} timeout={1000}>
+              <h1 id="title" style={{margin: 0, fontSize: isMobileView ? '4rem' : '7rem'}}>
+                Winds Of Shadow
+              </h1>
+            </Grow>
+            <Grow in={true} timeout={1000}>
+              <p style={{position: "relative", fontSize: isMobileView ? '1.5rem' : '2rem', margin: 0}}>By Benjamin J Stegenga</p>
+            </Grow>
+          </Box>
       </Box>
 
       <Box sx={descriptionContainer}>
@@ -89,18 +107,18 @@ function App() {
           minHeight: '500px'
         }}>
           <Box sx={centerContent}>
-            <h2>About the Book</h2>
+            <h2 id="about-the-book" >About the Book</h2>
           </Box>
           <p>A world hanging on the edge of chaos and darkness lies before Sean. One cold fateful night in the deep of winter a warrior named Taylor stumbles upon Sean alone in the mountains. Their meeting triggers Sean toward adventure in the world sprawled before him, one they must protect. At each step in the journey Sean faces task after task meeting Ancient’s and exploring the Southern Wasted Lands.</p>
           <p>It is here that Sean pieces together who exactly the Proclaimer of Light is; the person destined to tip the scale of an eternal struggle of good versus evil. Can Sean discover what it means to be Ancient and save the world? Joining the war of Light against Dark, Sean unlocks the shrouded mystery of the hero: Orwick Stormbringer.</p>
         </Box>
       </Box>
 
-      <Box sx={{...parallax, height: '300px', backgroundImage: `url(${sectionTwoBackground})`}} />
+      <Box sx={{...parallax, height: '300px', backgroundImage: `url(${darkForest})`, filter: 'brightness(0.75)'}} />
 
       <Container sx={descriptionContainer}>
         <Box sx={centerContent}>
-          <h2>Get a Copy</h2>
+          <h2 id="get-a-copy">Get a Copy</h2>
         </Box>
         <Grid container spacing={2} sx={{padding: '2rem 0'}}>
           <Grid item sx={centerContent} md={6} xs={12}>
@@ -112,18 +130,20 @@ function App() {
         </Grid>
       </Container>
 
-      <Box sx={{...parallax, height: '300px', backgroundImage: `url(${sectionTwoBackground})`}} />
+      <Box sx={{...parallax, height: '300px', backgroundImage: `url(${cavern})`, filter: 'brightness(0.75)'}} />
 
       <Container sx={descriptionContainer}>
         <Box sx={centerContent}>
-          <h2>About the Author</h2>
+          <h2 id="about-the-author">About the Author</h2>
         </Box>
         <Grid container spacing={2} sx={{padding: '2rem 0'}}>
           <Grid item sx={centerContent} md={6} xs={12}>
-            <img src={authorPhoto} alt="Winds of Shadow author Benjamin J Stegenga" />
+            <Box sx={isMobileView ? photoCircle : {}}>
+              <img src={authorPhoto} alt="Winds of Shadow author Benjamin J Stegenga" />
+            </Box>
           </Grid>
           <Grid item sx={centerContent} md={6} xs={12}>
-            <Box sx={{...centerContent, flexDirection: 'column', padding: '0 1.5rem'}}>
+            <Box sx={{...centerContent, flexDirection: 'column', padding: '0 1.5rem', textAlign: 'center'}}>
               <p>Benjamin J Stegenga</p>
               <p>At a young age Ben took to reading like a bird to hunting worms. Each book conquered grew his imagination until one day reading simply did not seem enough. Taking pen in hand a story was written, the likes of which Ben had been wanting to read for years. Now as a published author to Winds of Shadow, once more Ben will take up pen to continue on the story and prepare book 2 in the series for release.</p>
             </Box>
