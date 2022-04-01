3809cd36b55218aafe90e3d7770b796ee51eccac
Glen Simon <glen.a.simon@gmail.com>
2022-04-01 12:06:31 -0400

    Shrink nav spacing on mobile

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,46 +1,47 @@
-import { Box, IconButton, Menu, MenuItem } from '@mui/material';
+import { Box, IconButton, Menu, MenuItem, useMediaQuery } from '@mui/material';
 import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const NavButton: React.FC<INavButtonProps> = ({scrollRef}) => {
-   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-   const open = Boolean(anchorEl);
+  const isMobileView = useMediaQuery(`(max-width:900px)`);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
-   const handleMenuButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
+  const handleMenuButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const onMenuItemClick = (elementId: string) => {
-      const element = document.getElementById(elementId);
-      scrollRef?.current?.scrollTo({left: 0, top: element?.offsetTop ?? 0, behavior: "smooth"});
-      handleMenuClose();
-    }
+  const onMenuItemClick = (elementId: string) => {
+    const element = document.getElementById(elementId);
+    scrollRef?.current?.scrollTo({left: 0, top: element?.offsetTop ?? 0, behavior: "smooth"});
+    handleMenuClose();
+  }
 
-    const handleMenuClose = () => {
-      setAnchorEl(null);
-    }
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  }
 
-   return (
-      <Box sx={{position: 'absolute', right: '1.75rem', background: '#858282', zIndex: 1, top: '0.5rem', boxShadow: 3, borderRadius: '0.5rem'}}>
-        <IconButton id='navigation-button' aria-label='Navigation Menu' size='large' onClick={handleMenuButtonClick} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} aria-controls={open ? 'navigation-menu' : undefined}>
-          <MenuIcon />
-        </IconButton>
-        <Menu
-          id='navigation-menu'
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleMenuClose}
-          MenuListProps={{
-            'aria-labelledby': 'navigation-button',
-          }}
-        >
-          <MenuItem onClick={() => onMenuItemClick('title')}>Title</MenuItem>
-          <MenuItem onClick={() => onMenuItemClick('about-the-book')}>About the Book</MenuItem>
-          <MenuItem onClick={() => onMenuItemClick('get-a-copy')}>Get a Copy</MenuItem>
-          <MenuItem onClick={() => onMenuItemClick('about-the-author')}>About the Author</MenuItem>
-        </Menu>
-      </Box>
-   );
+  return (
+    <Box sx={{position: 'absolute', right: isMobileView ? '1.25rem' : '1.75rem', background: '#858282', zIndex: 1, top: '0.5rem', boxShadow: 3, borderRadius: '0.5rem'}}>
+      <IconButton id='navigation-button' aria-label='Navigation Menu' size='large' onClick={handleMenuButtonClick} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} aria-controls={open ? 'navigation-menu' : undefined}>
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id='navigation-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleMenuClose}
+        MenuListProps={{
+          'aria-labelledby': 'navigation-button',
+        }}
+      >
+        <MenuItem onClick={() => onMenuItemClick('title')}>Title</MenuItem>
+        <MenuItem onClick={() => onMenuItemClick('about-the-book')}>About the Book</MenuItem>
+        <MenuItem onClick={() => onMenuItemClick('get-a-copy')}>Get a Copy</MenuItem>
+        <MenuItem onClick={() => onMenuItemClick('about-the-author')}>About the Author</MenuItem>
+      </Menu>
+    </Box>
+  );
 };
 
 export interface INavButtonProps {
